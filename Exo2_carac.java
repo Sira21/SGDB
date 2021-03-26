1aae07006cebf60e878cf61e5d2387553d00e73a
EspoirNorbert <espoirnorbert@gmail.com>
2021-03-26 17:38:35 +0100

    ajout d'une nouvelle ligne from espoir

--- Exo2_carac.java
+++ Exo2_carac.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-public class Exo2_carac{
-	public static void main(String args[]){
+
+public class Exo2_carac {
+	public static void main(String args[]) {
 
 		Scanner sc = new Scanner(System.in);
 		String carac;
 		System.out.print("Saisissez une chaine de caractére : ");
 		carac = sc.next();
 		int taille = carac.length();
-		for(int i = 0; i < taille; i=i+2){
-			System.out.print(carac.charAt(i)+"  ");	
+		for (int i = 0; i < taille; i = i + 2) {
+			System.out.print(carac.charAt(i) + "  ");
 		}
 		System.out.println();
 		System.out.println("Ceci est faux");
-		System.out.println(carac.charAt(0)+"  "+carac.charAt(taille-1));
+		// ajout de la ligne de code
+		System.out.println("Test venant d'un collaborateur git");
+
+		System.out.println(carac.charAt(0) + "  " + carac.charAt(taille - 1));
 	}
 }
